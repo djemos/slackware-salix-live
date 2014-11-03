8d6c03f3330cee75462e8d522c0c365aa871f7ee
Dimitris Tzemos <djemos@salixos.org>
2014-11-04 00:48:01 +0200

    Update README.install_on_usb.sh

--- common/install_on_usb/README.install_on_usb.sh
+++ common/install_on_usb/README.install_on_usb.sh
@@ -1,40 +1,62 @@
-Installing iso image to a usb key.
+INSTALLING THE ISO IMAGE TO A USB KEY
+=====================================
 
-install_on_usb.sh script is used to install iso image to a usb key using the iso image itself.
-You can mount the iso image to a directory e.g. /tmp/iso and enter to boot folder to this directory e.g. cd /tmp/iso/boot
-and execute from there the script
+The install_on_usb.sh script is used to install the iso image to a usb
+key using the iso image itself.
 
-sudo mkdir /tmp/iso
-sudo mount -o loop path_to_iso_image /tmp/iso
-cd /tmp/iso/boot
-sudo sh install_on_usb.sh --usb path_to_iso_image device
+You can mount the iso image to a directory e.g.: /tmp/iso and enter
+the boot directory within the mountpoint, e.g.: cd /tmp/iso/boot
+and execute the script from there:
 
-device can be /dev/sdb or /dev/sdc according to how many usb keys are pluged in your system.
-There is no chance to install to your hard disk /dev/sda or /dev/hda. Script will exit.
+  sudo mkdir /tmp/iso
+  sudo mount -o loop path_to_iso_image /tmp/iso
+  cd /tmp/iso/boot
+  sudo sh install_on_usb.sh --usb path_to_iso_image device
 
-So, you have to specify path to iso image and device. The script will ask user to confirm the device specified 
-and it will also optionally create a persistent ext3 file.
-If user did not do so then can create a persistent file using --persistent option.
-There is no need to specify path to iso because Salix Live is already installed to usb key.
-However, you do need to specify the architecture, because this affects the location of the persistent file.
+device can be something like /dev/sdb or /dev/sdc according to how
+many usb keys are plugged in your system.
+There is no chance to install to your hard disk /dev/sda or /dev/hda.
+The script will just exit in such a case.
 
-sudo sh install_on_usb.sh --persistent 32|64 device
+So, you have to specify the path to the iso image and device. The script
+will ask the user to confirm the device specified and it will also
+optionally create a persistent ext3 file.
+
+If the user did not do so on first installation, then he can create a
+persistent file using the --persistent option. There is now no need to
+specify the path to the iso because Salix Live is already installed to
+the usb key. However, you do need to specify the architecture, because
+this affects the location of the persistent file:
+
+  sudo sh install_on_usb.sh --persistent 32|64 device
+
+For example, for a 64-bit iso installed on /dev/sdc, you should
+run the following command to create persistence:
+
+  sudo sh install_on_usb.sh --persistent 64 /dev/sdc
 
 
-Typing sh install_on_usb.sh --help get the following help message
+Typing:
+ 
+  sh install_on_usb.sh --help
 
-install_on_usb.sh [-h/--help] [-v/--version]
- -h, --help: this usage message
- -v, --version: the version author and licence
+will output the following help message:
 
-install_on_usb.sh --usb isoname device
-install_on_usb.sh --persistent 32|64 device
+  install_on_usb.sh [-h/--help] [-v/--version]
+  -h, --help: this usage message
+  -v, --version: the version author and licence
 
--> --usb option installs syslinux on a USB key using an ISO (specify path to image and device)
--> The script will ask user to confirm the device specified
--> It will also optionally create a persistent ext3 file.
+  install_on_usb.sh --usb isoname device
+  install_on_usb.sh --persistent 32|64 device
 
--> --persistent option creates a persistent ext3 file after installation, if user did not do so then
--> specify architecture and device
--> No need to specify path to iso because Salix Live is already installed
+  -> --usb option installs syslinux on a USB key using an ISO (specify
+       path to image and device)
+  -> The script will ask user to confirm the device specified
+  -> It will also optionally create a persistent ext3 file.
 
+  -> --persistent option creates a persistent ext3 file after
+     installation, if user did not do so then
+  -> specify architecture and device
+  -> No need to specify path to iso because Salix Live is already
+     installed
+     
